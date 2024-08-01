4079223a4200e5577e019f0676cbf5a5c7c070d3
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-08-01 18:38:36 +0100

    refactored code

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const formattedDate =
     now.toISOString().slice(0, 10) + " " + now.toTimeString().slice(0, 5);
 
-  // const datePickers = document.querySelectorAll(".datetime-picker");
-  // datePickers.forEach((picker) => {
-  //   picker.placeholder = formattedDate;
-  // });
   flatpickr(".datetime-picker", {
     enableTime: true,
     dateFormat: "Y-m-d H:i",
@@ -23,14 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     },
   });
+
+  // Pastabos textarea expansion
+  const textarea = document.getElementById("pastabos");
+
+  textarea.addEventListener("input", autoResize);
+
+  function autoResize() {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+  }
 });
-
-// Pastabos textarea expansion
-const textarea = document.getElementById("pastabos");
-
-textarea.addEventListener("input", autoResize);
-
-function autoResize() {
-  this.style.height = "auto";
-  this.style.height = this.scrollHeight + "px"; //
-}
